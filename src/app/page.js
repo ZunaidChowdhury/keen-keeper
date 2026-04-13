87b74e1b80816133c79d94c3a8007af708efa417
Zunaid <133950183+ZunaidChowdhury@users.noreply.github.com>
2026-04-13 21:30:32 +0600

    project setup done

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-500">Hello</h1>
+      <button className="btn btn-primary">Primary</button>
+    </div>
+  );
+}
